47631f4ea648c55db23f93e40aba2f0c94a8c444
Glenn Mark Flores <engrflare@gmail.com>
2020-11-04 21:54:34 +0800

    Update main.py

--- Final/main.py
+++ Final/main.py
@@ -1,87 +1,73 @@
-# Psuedo Code: Add every word of the file to a string, then add all interesting words
-# to a list, lastly count how many times the word repeats using a dictionary and add to cloud.
-
 import wordcloud
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-# Get the File
-fname = input("Enter File Name: ")
-
-try:
-	fread = open(fname)
-	file = fread.read()
-except Exception as e:
-	print(e)
-
-
 def image_name(name):
-	dot = name.find('.')
-	img_name = name[:dot]
-	return "%s.jpg" % (img_name)
+    """Get the name of the file and returns a string with .jpg format"""
+    
+    # Gets the '.' position
+    dot = name.find('.')
+    # Slice the name from beginning and before '.'
+    img = name[:dot]
+    # return string with jpg format
+    return "{}.jpg".format(img)
 
 
-def get_interesting_words(file):
-	# Here is a list of punctuations and uninteresting words you can use to process your text
-    # punctuations = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
+def calculate_frequencies(file_contents):
+    """Checks the contents of the file and calulcate
+     the occurences of words and returns a dictionary """
+
+    # list of uninteresting words
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
-
-    # To store the strings
-    file_contents = ""
-
-    # Enumerate the file
-    for index, char in enumerate(file):
-    	# Check for word
-        if char.isalpha() == True or char.isspace():
-			# Concatenate every word 
-            file_contents += char
-
-    # Split every word
-    spit_contents = file_contents.split()
-
-    # loop every word in the list. Check lower case words. Add the interesting words to list
-    interesting_words = [item for item in spit_contents if item.lower() not in uninteresting_words and item.isalpha() == True]
-
-    return interesting_words
-
-
-def calculate_frequencies(words):
-    # Dictionary to store to word counts
+    
+    # Dictionary to store frequencies
     frequencies = {}
 
-    # loop words in the list
-    for word in words:
-        # if word is not in the list. Add 1 value
-        # Add count for every word repeatition
-        frequencies[word.lower()] = frequencies.get(word, 0) + 1
+    # Split the contents into words
+    file_contents = file_contents.split()
 
+    # loop through every word
+    for line in file_contents:
+        # Contatenate every word
+        words = ''.join(w for w in line if w.isalpha())
+        # Check if word is not a uninetersting_words
+        if words.lower() not in uninteresting_words:
+            # Get the number of occurences
+            frequencies[words.lower()] = frequencies.get(words, 0) + 1
+
+    # returns the frequencies from words
     return frequencies
 
 
 def generate_word_cloud(frequencies):
-	# Instantiate word cloud
+    """Gets the dictionary from calcualte_frequencies and returns a numpy array  """
+
+    # Instantiate word cloud
     cloud = wordcloud.WordCloud()
-    # Generate the word cloud
+    # Create a word_cloud from words and frequencie
     cloud.generate_from_frequencies(frequencies)
-    # Create the word cloud image image
+    # Export to image file.
     cloud.to_file(image_name(fname))
-    # Return the word cloud
+    # returns a numpy array
     return cloud.to_array()
 
 
-words = get_interesting_words(file)
+# Get the File
+try:
+    fname = input("Enter File Name: ")
+    fread = open(fname)
+    file = fread.read()
+except Exception as e:
+    print(e)
+else:  
+    frequencies = calculate_frequencies(file)
+    myimage = generate_word_cloud(frequencies)
 
-print(words)
-
-file_frequencies = calculate_frequencies(words)
-
-myimage = generate_word_cloud(file_frequencies)
-
-plt.imshow(myimage, interpolation = 'nearest')
-plt.axis('off')
-plt.show()
+    # Display an image
+    plt.imshow(myimage, interpolation = 'nearest')
+    plt.axis('off')
+    plt.show()
